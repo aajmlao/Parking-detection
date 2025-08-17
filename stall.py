f2899ab00bb8bddb8b9d32462186753e9fd32a71
Aaron Lao <91438480+aajmlao@users.noreply.github.com>
2025-08-16 21:32:09 -0700

    Update stall.py
    Not finished the logic of Stall class

--- stall.py
+++ stall.py
@@ -1,16 +1,18 @@
 import cv2, time
 
 class Stall:
-    def __init__(self, single_stall_coordination, predicted_objects):
+    def __init__(self, id, single_stall_coordination):
+        # Init values for the stall
+        self.id = id 
         self.stall_coord = single_stall_coordination
-        self.objects_coord = predicted_objects[0].boxes.xyxy.cpu().numpy()
-        self.objects_confidents = predicted_objects[0].boxes.conf.cpu().numpy()
+        # store the occupied coordination from 
         self.occupied_coord = None
-        self.is_stall_occupied = False
-        self.stall_state = {"current_state": None, "current_state_start_time": None,
-                            "predicted_state": None, "predicted_state_start_time": None}
-        self.__stall_occupation_status()
-
+        
+        self.current_state = None
+        self.current_state_start_time = None
+        self.predicted_state = None
+        self.predicted_state_start_time = None
+        
 
     def __center_in_xyxy(self, object_coordination) -> bool:
         x1, y1, x2, y2 = object_coordination
@@ -21,23 +23,24 @@ class Stall:
 
         return (marked_x1 < center_x < marked_x2) and (marked_y1 < center_y < marked_y2)
 
-    def __stall_occupation_status(self): 
-        for coord in self.objects_coord.astype(int):
+    def stall_occupation_status(self, objects_coordination): 
+        
+        for coord in objects_coordination.astype(int):
             if self.__center_in_xyxy(coord):
                 self.occupied_coord = coord
-                self.is_stall_occupied = True
+                if self.current_state is None:
+                    self.current_state = True
+                else:
+                    self.predicted_state = True
                 break
-
-        # initializing the stall state
+        # After the checking all predicted objects
+        if self.current_state is None:
+            self.current_state = False
+        else:
+            self.predicted_state = False
+ 
         now = time.monotonic()
         
-        if all(value is None for value in self.stall_state.values()):
-            self.stall_state["current_state"] = self.is_stall_occupied
-            self.stall_state["current_state_start_time"] = now
-            self.stall_state["predicted_state"] = self.is_stall_occupied
-            self.stall_state["predicted_state_start_time"] = now
-        else:
-            self.__update_on_stall(now)
 
 
     def get_stall_coordination(self) -> list:
@@ -59,5 +62,5 @@ class Stall:
     def __update_on_stall(self, now_time):
         pass
 
-    def get_stall_state(self) -> dict:
-        return self.stall_state
+    # def get_stall_state(self) -> dict:
+    #     return self.stall_state
